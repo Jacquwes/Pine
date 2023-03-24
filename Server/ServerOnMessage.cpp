4fe5ca5fc5155677f0e68168929d270000dafb9a
Jacquwes <38167139+Jacquwes@users.noreply.github.com>
2023-03-24 15:00:03 +0100

    Add unacceptable messages type to server message handler

--- Server/ServerOnMessage.cpp
+++ Server/ServerOnMessage.cpp
@@ -11,5 +11,30 @@ AsyncTask Server::OnMessage(std::shared_ptr<Connection> const& client, std::shar
 {
 	std::cout << "  Message received from client: " << std::dec << client->GetId() << std::endl;
 
+	switch (message->header.messageType)
+	{
+		using enum SocketMessages::MessageType;
+
+	case AcknowledgeMessage:
+		[[fallthroug]]
+	case ErrorMessage:
+		[[fallthroug]]
+	case ReceiveChatMessage:
+		std::cout << "  Message type not expected from client: " << std::dec << static_cast<int>(message->header.messageType) << std::endl;
+		DisconnectClient(client->GetId());
+		break;
+
+	case HelloMessage:
+		[[fallthroug]]
+	case IdentifyMessage:
+		std::cout << "  Message type not expected from client now: " << std::dec << static_cast<int>(message->header.messageType) << std::endl;
+		break;
+
+	default:
+		std::cout << "  Unknown message type: " << std::dec << static_cast<int>(message->header.messageType) << std::endl;
+		DisconnectClient(client->GetId());
+		break;
+	}
+
 	co_return;
 }

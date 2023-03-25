eb5e7688631a96e5810a91bfb4d9083095693f30
Jacquwes <38167139+Jacquwes@users.noreply.github.com>
2023-03-25 12:18:54 +0100

    Create server integration tests project

---
+++ ServerIntegrationTests/EstablishConnectionIntegrationTests.cpp
@@ -0,0 +1,15 @@
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ServerIntegrationTests
+{
+	TEST_CLASS(EstablishConnection)
+	{
+	public:
+		
+		TEST_METHOD(ValidateConnection)
+		{
+		}
+	};
+}

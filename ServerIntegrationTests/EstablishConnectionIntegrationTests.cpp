e6a827e15cb0f449bbb14db9a2fdbcdb1054e98a
Jacquwes <38167139+Jacquwes@users.noreply.github.com>
2023-03-30 20:21:24 +0200

    Add utilitary function

--- ServerIntegrationTests/EstablishConnectionIntegrationTests.cpp
+++ ServerIntegrationTests/EstablishConnectionIntegrationTests.cpp
@@ -10,7 +10,7 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace ServerIntegrationTests
 {
-	TEST_CLASS(EstablishConnection)
+	TEST_CLASS(EstablishConnectionIntegrationTests)
 	{
 	public:
 

c82ae031d64832fe6868f987ba6f66c31097ff2a
Jacques <paul.hariel@outlook.com>
2023-09-07 23:10:48 +0200

    Rename snowflake_test to snowflake

--- tests/unit/snowflake_tests.cpp
+++ tests/unit/snowflake_tests.cpp
@@ -6,7 +6,7 @@
 
 #include "snowflake.h"
 
-TEST(snowflake_test, default_constructor)
+TEST(snowflake, default_constructor)
 {
 	pine::snowflake snowflake;
 	EXPECT_EQ(snowflake.value.worker_id, 0);
@@ -14,7 +14,7 @@ TEST(snowflake_test, default_constructor)
 	EXPECT_EQ(snowflake.value.sequence, 0);
 }
 
-TEST(snowflake_test, constructor)
+TEST(snowflake, constructor)
 {
 	pine::snowflake snowflake(0x0000000000000001);
 	EXPECT_EQ(snowflake.value.timestamp, 0);
@@ -23,7 +23,7 @@ TEST(snowflake_test, constructor)
 	EXPECT_EQ(snowflake.value.sequence, 1);
 }
 
-TEST(snowflake_test, assignment_operator)
+TEST(snowflake, assignment_operator)
 {
 	pine::snowflake snowflake;
 	snowflake = 0x0000000000000001;
@@ -33,39 +33,39 @@ TEST(snowflake_test, assignment_operator)
 	EXPECT_EQ(snowflake.value.sequence, 1);
 }
 
-TEST(snowflake_test, equality_operator)
+TEST(snowflake, equality_operator)
 {
 	pine::snowflake snowflake1(0x0000000000000001);
 	pine::snowflake snowflake2(0x0000000000000001);
 	EXPECT_TRUE(snowflake1 == snowflake2);
 }
 
-TEST(snowflake_test, inequality_operator)
+TEST(snowflake, inequality_operator)
 {
 	pine::snowflake snowflake1(0x0000000000000001);
 	pine::snowflake snowflake2(0x0000000000000002);
 	EXPECT_TRUE(snowflake1 != snowflake2);
 }
 
-TEST(snowflake_test, equality_operator_with_uint64_t)
+TEST(snowflake, equality_operator_with_uint64_t)
 {
 	pine::snowflake snowflake(0x0000000000000001);
 	EXPECT_TRUE(snowflake == (uint64_t)0x0000000000000001);
 }
 
-TEST(snowflake_test, inequality_operator_with_uint64_t)
+TEST(snowflake, inequality_operator_with_uint64_t)
 {
 	pine::snowflake snowflake(0x0000000000000001);
 	EXPECT_TRUE(snowflake != (uint64_t)0x0000000000000002);
 }
 
-TEST(snowflake_test, cast_to_uint64_t)
+TEST(snowflake, cast_to_uint64_t)
 {
 	pine::snowflake snowflake(0x0000000000000001);
 	EXPECT_EQ((uint64_t)snowflake, 0x0000000000000001);
 }
 
-TEST(snowflake_test, stream_operator)
+TEST(snowflake, stream_operator)
 {
 	pine::snowflake snowflake(0x0000000000000001);
 	std::stringstream stream;

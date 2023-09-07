b7259807d8167613b7d31e79f37a2ec5a823532c
Jacques <paul.hariel@outlook.com>
2023-09-07 20:22:23 +0200

    Document

--- shared/include/snowflake.h
+++ shared/include/snowflake.h
@@ -5,12 +5,24 @@
 
 namespace pine
 {
+	/// <summary>
+	/// Represents a unique identifier for an object.
+	/// </summary>
 	struct snowflake
 	{
+		/// <summary>
+		/// Creates a new snowflake with a new generated id.
+		/// </summary>
 		snowflake();
 
+		/// <summary>
+		/// Creates a new snowflake from an id.
+		/// </summary>
 		explicit snowflake(uint64_t const& id);
 
+		/// <summary>
+		/// The value of the snowflake.
+		/// </summary>
 		struct
 		{
 			uint64_t timestamp : 42;
@@ -19,6 +31,9 @@ namespace pine
 			uint64_t sequence : 12;
 		} value;
 
+		/// <summary>
+		/// Creates a new snowflake from an id.
+		/// </summary>
 		snowflake operator=(uint64_t const& id);
 
 		bool operator !=(snowflake const& other) const;
@@ -29,8 +44,15 @@ namespace pine
 
 		explicit(false) constexpr operator uint64_t() const;
 
+		/// <summary>
+		/// Writes the snowflake to the output stream.
+		/// </summary>
 		friend std::ostream& operator<<(std::ostream& out, snowflake const& snowflake);
 
+	private:
+		/// <summary>
+		/// The last millisecond a snowflake was generated.
+		/// </summary>
 		static uint64_t last_millisecond;
 	};
 }

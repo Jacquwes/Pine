661b23707865046dee453b98226ba32c17dc6029
Jacques <paul.hariel@outlook.com>
2023-09-07 22:46:19 +0200

    Move message to cmake project

---
+++ shared/src/message.cpp
@@ -0,0 +1,59 @@
+#include <cstdint>
+#include <vector>
+
+#include "message.h"
+#include "snowflake.h"
+
+namespace pine::socket_messages
+{
+	message_header::message_header(std::vector<uint8_t> const& buffer)
+	{
+		parse(buffer);
+	}
+
+	void message_header::parse(std::vector<uint8_t> const& buffer)
+	{
+		std::memcpy(&body_size, &buffer[1], sizeof(body_size));
+
+		type_of_message = static_cast<message_type>(buffer[0]);
+
+
+		uint64_t id;
+		std::memcpy(&id, &buffer[sizeof(type_of_message) + sizeof(body_size)], sizeof(id));
+		message_id = snowflake{ id };
+	}
+
+	std::vector<uint8_t> message_header::serialize() const
+	{
+		std::vector<uint8_t> buffer;
+
+		buffer.push_back(static_cast<uint8_t>(type_of_message));
+		buffer.resize(size);
+
+		// copy body size
+		std::memcpy(&buffer[sizeof(type_of_message)], &body_size, sizeof(body_size));
+
+		// copy message id
+		uint64_t id = message_id;
+
+		std::memcpy(&buffer[sizeof(type_of_message) + sizeof(body_size)], &id, sizeof(id));
+		
+		return buffer;
+	}
+
+	bool message::parse_body(std::vector<uint8_t> const& buffer)
+	{
+		return false;
+	}
+
+	std::vector<uint8_t> message::serialize() const
+	{
+		std::vector<uint8_t> buffer(message_header::size, 0);
+		return buffer;
+	}
+
+	uint64_t message::get_body_size() const
+	{
+		return 0;
+	}
+}

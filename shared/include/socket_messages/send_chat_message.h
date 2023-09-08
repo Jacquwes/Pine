4390d762a19b9338f9a4c4ebf51b53905e6da900
Jacques <paul.hariel@outlook.com>
2023-09-08 13:11:14 +0200

    Implement send_chat_message

--- shared/include/socket_messages/send_chat_message.h
+++ shared/include/socket_messages/send_chat_message.h
@@ -1,80 +1,25 @@
 #pragma once
 
-#include <array>
-#include <bit>
 #include <cstdint>
+#include <string>
 #include <vector>
 
-#include "Message.h"
-#include "User.h"
+#include "message.h"
 
-namespace SocketMessages
+namespace pine::socket_messages
 {
-	struct SendChatMessage : Message
+	struct send_chat_message : message
 	{
-		SendChatMessage()
-		{
-			header.messageType = MessageType::SendChatMessage;
-			header.bodySize = GetBodySize();
-		}
+		send_chat_message();
 
-		bool ParseBody(std::vector<uint8_t> const& buffer) override
-		{
-			if (buffer.size() < sizeof(m_chatMessageLength) + ChatMessageMinLength
-				|| buffer.size() > sizeof (m_chatMessageLength) + ChatMessageMaxLength)
-				return false;
+		bool parse_body(std::vector<uint8_t> const& buffer) override;
 
-			std::memcpy(std::bit_cast<uint8_t*>(&m_chatMessageLength),
-						buffer.data(), sizeof(m_chatMessageLength));
+		std::vector<uint8_t> serialize() const override;
 
-			if (buffer.size() != sizeof(m_chatMessageLength) + m_chatMessageLength)
-				return false;
+		uint64_t constexpr get_body_size() const final;
 
-			m_chatMessage = std::string(buffer.begin() + sizeof(m_chatMessageLength),
-										buffer.begin() + sizeof(m_chatMessageLength) + m_chatMessageLength);
+		constexpr bool check_message_content(std::string_view const& content);
 
-			header.bodySize = GetBodySize();
-
-			return true;
-		}
-
-		std::vector<uint8_t> Serialize() const override
-		{
-			std::vector<uint8_t> buffer(MessageHeader::size + GetBodySize(), 0);
-			std::vector<uint8_t> headerBuffer = header.Serialize();
-
-			uint8_t cursor = 0;
-
-			std::memcpy(&buffer[cursor], &headerBuffer[0], MessageHeader::size);
-			cursor += MessageHeader::size;
-			std::memcpy(&buffer[cursor], &m_chatMessageLength, sizeof(m_chatMessageLength));
-			cursor += sizeof(m_chatMessageLength);
-			std::memcpy(&buffer[cursor], &m_chatMessage[0], m_chatMessageLength);
-
-			return buffer;
-		}
-
-		uint64_t constexpr GetBodySize() const final
-		{
-			return sizeof(m_chatMessageLength) + m_chatMessageLength;
-		}
-
-		[[nodiscard]] constexpr std::string const& GetChatMessage() const { return m_chatMessage; }
-		constexpr bool SetChatMessage(std::string_view const& chatMessage)
-		{
-			if (chatMessage.size() > ChatMessageMaxLength || chatMessage.size() < ChatMessageMinLength)
-				return false;
-
-			m_chatMessage = chatMessage;
-			m_chatMessageLength = static_cast<uint8_t>(chatMessage.size());
-
-			header.bodySize = GetBodySize();
-
-			return true;
-		}
-
-	private:
-		uint16_t m_chatMessageLength{};
-		std::string m_chatMessage{};
+		std::string message_content{};
 	};
 }

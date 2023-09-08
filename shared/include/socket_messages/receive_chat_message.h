50022a5a328396516db584cc596a320fde23ee91
Jacques <paul.hariel@outlook.com>
2023-09-08 12:45:22 +0200

    Implement receive_chat_message

--- shared/include/socket_messages/receive_chat_message.h
+++ shared/include/socket_messages/receive_chat_message.h
@@ -2,113 +2,27 @@
 
 #include <bit>
 #include <cstdint>
+#include <string>
 #include <vector>
 
-#include "Message.h"
+#include "message.h"
 
-namespace SocketMessages
+namespace pine::socket_messages
 {
-	struct ReceiveChatMessage : Message
+	struct receive_chat_message : message
 	{
-		ReceiveChatMessage()
-		{
-			header.messageType = MessageType::ReceiveChatMessage;
-			header.bodySize = GetBodySize();
-		}
+		receive_chat_message();
 
-		bool ParseBody(std::vector<uint8_t> const& buffer) override
-		{
-			if (buffer.size() < sizeof(m_authorUsernameLength) + UsernameMinLength + sizeof(m_chatMessageLength) + ChatMessageMinLength)
-				return false;
+		bool parse_body(std::vector<uint8_t> const& buffer) override;
 
-			if (buffer.size() > sizeof(m_authorUsernameLength) + UsernameMaxLength + sizeof(m_chatMessageLength) + ChatMessageMaxLength)
-				return false;
+		std::vector<uint8_t> serialize() const override;
 
-			std::memcpy(std::bit_cast<uint8_t*>(&m_authorUsernameLength),
-						buffer.data(),
-						sizeof(m_authorUsernameLength));
+		uint64_t get_body_size() const final;
 
-			if (buffer.size() < sizeof(m_authorUsernameLength) + m_authorUsernameLength + sizeof(m_chatMessageLength) + ChatMessageMinLength)
-				return false;
+		constexpr bool check_author_username(std::string_view const& author_username);
+		constexpr bool check_message_content(std::string_view const& message_content);
 
-			m_authorUsername = std::string(buffer.begin() + sizeof(m_authorUsernameLength),
-										   buffer.begin() + sizeof(m_authorUsernameLength) + m_authorUsernameLength);
-
-			std::memcpy(std::bit_cast<uint16_t*>(&m_chatMessageLength),
-						buffer.data() + sizeof(m_authorUsernameLength) + m_authorUsernameLength,
-						sizeof(m_chatMessageLength));
-
-			if (buffer.size() != sizeof(m_authorUsernameLength) + m_authorUsernameLength + sizeof(m_chatMessageLength) + m_chatMessageLength)
-				return false;
-
-			m_chatMessage = std::string(buffer.begin() + sizeof(m_authorUsernameLength) + m_authorUsernameLength + sizeof(m_chatMessageLength),
-										buffer.end());
-
-			header.bodySize = GetBodySize();
-
-			return true;
-		}
-
-		std::vector<uint8_t> Serialize() const override
-		{
-			std::vector<uint8_t> buffer(MessageHeader::size + GetBodySize(), 0);
-			std::vector<uint8_t> headerBuffer = header.Serialize();
-
-			uint8_t cursor = 0;
-
-			std::memcpy(&buffer[cursor], &headerBuffer[0], MessageHeader::size);
-			cursor += MessageHeader::size;
-			std::memcpy(&buffer[cursor], &m_authorUsernameLength, sizeof(m_authorUsernameLength));
-			cursor += sizeof(m_authorUsernameLength);
-			std::memcpy(&buffer[cursor], &m_authorUsername[0], m_authorUsernameLength);
-			cursor += m_authorUsernameLength;
-			std::memcpy(&buffer[cursor], &m_chatMessageLength, sizeof(m_chatMessageLength));
-			cursor += sizeof(m_chatMessageLength);
-			std::memcpy(&buffer[cursor], &m_chatMessage[0], m_chatMessageLength);
-
-			return buffer;
-		}
-
-		uint64_t GetBodySize() const final
-		{
-			return sizeof(m_authorUsernameLength)
-				+ m_authorUsernameLength
-				+ sizeof(m_chatMessageLength)
-				+ m_chatMessageLength;
-		}
-
-		[[nodiscard]] constexpr std::string const& GetAuthorUsername() const { return m_authorUsername; }
-		constexpr bool SetAuthorUsername(std::string_view const& authorUsername)
-		{
-			if (authorUsername.length() < UsernameMinLength || authorUsername.length() > UsernameMaxLength)
-				return false;
-
-			m_authorUsername = authorUsername;
-			m_authorUsernameLength = static_cast<uint8_t>(authorUsername.length());
-
-			header.bodySize = GetBodySize();
-
-			return true;
-		}
-
-		[[nodiscard]] constexpr std::string const& GetChatMessage() const { return m_chatMessage; }
-		constexpr bool SetChatMessage(std::string_view const& chatMessage)
-		{
-			if (chatMessage.length() < ChatMessageMinLength || chatMessage.length() > ChatMessageMaxLength)
-				return false;
-
-			m_chatMessage = chatMessage;
-			m_chatMessageLength = static_cast<uint16_t>(chatMessage.length());
-
-			header.bodySize = GetBodySize();
-
-			return true;
-		}
-
-	private:
-		uint8_t m_authorUsernameLength{};
-		std::string m_authorUsername{};
-		uint16_t m_chatMessageLength{};
-		std::string m_chatMessage{};
+		std::string author_username{};
+		std::string message_content{};
 	};
 }
---
+++ shared/src/socket_messages/receive_chat_message.cpp
@@ -0,0 +1,78 @@
+#include "socket_messages/receive_chat_message.h"
+
+namespace pine::socket_messages
+{
+	receive_chat_message::receive_chat_message()
+	{
+		header.type = message_type::RECEIVE_CHAT_MESSAGE;
+		header.body_size = get_body_size();
+	}
+
+	bool receive_chat_message::parse_body(std::vector<uint8_t> const& buffer)
+	{
+		if (buffer.size() < sizeof(uint8_t) + username_min_length + sizeof(uint16_t) + chat_message_min_length)
+			return false;
+
+		if (buffer.size() > sizeof(uint8_t) + username_max_length + sizeof(uint16_t) + chat_message_max_length)
+			return false;
+
+		uint8_t username_length = buffer[0];
+		if (buffer.size() < sizeof(uint8_t) + username_length + sizeof(uint16_t) + chat_message_min_length)
+			return false;
+
+		author_username = std::string(buffer.begin() + sizeof(uint8_t), buffer.begin() + sizeof(uint8_t) + username_length);
+
+		uint16_t message_length = *reinterpret_cast<uint16_t const*>(buffer.data() + sizeof(uint8_t) + username_length);
+		if (buffer.size() != sizeof(uint8_t) + username_length + sizeof(uint16_t) + message_length)
+			return false;
+
+		message_content = std::string(buffer.begin() + sizeof(uint8_t) + username_length + sizeof(uint16_t), buffer.end());
+
+		header.body_size = get_body_size();
+
+		return true;
+	}
+
+	std::vector<uint8_t> receive_chat_message::serialize() const
+	{
+		std::vector<uint8_t> buffer(message_header::size + get_body_size(), 0);
+		std::vector<uint8_t>::iterator it = buffer.begin();
+
+		it = std::copy_n(header.serialize().begin(), message_header::size, it);
+		uint8_t author_username_length = author_username.size();
+		it = std::copy_n(reinterpret_cast<uint8_t const*>(&author_username_length), sizeof(uint8_t), it);
+		it = std::copy_n(author_username.begin(), author_username_length, it);
+		uint16_t message_length = message_content.size();
+		it = std::copy_n(reinterpret_cast<uint8_t const*>(&message_length), sizeof(uint16_t), it);
+		it = std::copy_n(message_content.begin(), message_length, it);
+
+		return buffer;
+	}
+
+	uint64_t receive_chat_message::get_body_size() const
+	{
+		return sizeof(uint8_t) + author_username.size() + sizeof(uint16_t) + message_content.size();
+	}
+
+	constexpr bool receive_chat_message::check_author_username(std::string_view const& username)
+	{
+		if (username.size() < username_min_length)
+			return false;
+
+		if (username.size() > username_max_length)
+			return false;
+
+		return true;
+	}
+
+	constexpr bool receive_chat_message::check_message_content(std::string_view const& content)
+	{
+		if (content.size() < chat_message_min_length)
+			return false;
+
+		if (content.size() > chat_message_max_length)
+			return false;
+
+		return true;
+	}
+}

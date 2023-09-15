f9c6ea968752c80875a42f792ac144fd6b609078
Paul Hariel <paul.hariel@outlook.com>
2023-09-15 19:04:32 +0200

    Remove const attribute

--- shared/include/connection.h
+++ shared/include/connection.h
@@ -19,7 +19,10 @@ namespace pine
 		virtual ~connection();
 
 		async_operation<std::shared_ptr<socket_messages::message>> receive_message();
-		async_task send_message(std::shared_ptr<socket_messages::message> const& message) const;
+		async_task send_message(std::shared_ptr<socket_messages::message> const& message);
+
+		async_operation<std::vector<uint8_t>> receive_raw_message(uint64_t const& buffer_size);
+		async_task send_raw_message(std::vector<uint8_t> const& buffer);
 
 		async_operation<std::vector<uint8_t>> receive_raw_message(uint64_t const& buffer_size) const;
 		async_task send_raw_message(std::vector<uint8_t> const& buffer) const;

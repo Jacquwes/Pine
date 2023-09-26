5e5adb3f652b161e4b295ae859e2c769de79d026
Paul Hariel <paul.hariel@outlook.com>
2023-09-27 00:44:23 +0200

    Document user

--- shared/include/user.h
+++ shared/include/user.h
@@ -5,13 +5,20 @@
 
 namespace pine
 {
+	/// @brief A user of the chat application.
 	class user : public std::enable_shared_from_this<user>
 	{
 	public:
+		/// @brief Construct a user with an empty username.
 		user() = default;
+
+		/// @brief Construct a user with the given username.
 		user(std::string username);
+
+		/// @brief Destroy the user.
 		~user() = default;
 
+		/// @brief Username of the user.
 		std::string username{ "" };
 	};
 }
